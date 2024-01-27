500c29676178a10e87ee432d408b4edbf16b8c28
ColdCoder27 <155251105+ColdCoder27@users.noreply.github.com>
2024-01-27 21:22:36 +0100

    Finished 80% of frontEnd

--- app/history/page.jsx
+++ app/history/page.jsx
@@ -1,30 +1,56 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import { personsData } from '@/data/persons-data'
 import turing from "@/public/alanturning.jpg"
 import { useRouter } from 'next/navigation'
+import { IoIosSearch } from "react-icons/io";
+
 function History() {
+  const router = useRouter()
+  const [input, setInput] = useState("")
+  const handleChange = (e) => {
+    setInput(e.target.value)
+    console.log(input)
+  }
   const elements = personsData.map((obj)=>{
-    const router = useRouter()
     const person = obj.name
-    return(
-      <div onClick={()=>{router.push(`/history/${person.split(" ").join("")}`)}} key={obj.name} className='mt-10 w-72 h-72 bg-white rounded-xl flex flex-col items-center'>
-        <p className='text-center text-lg mt-5'>{person}</p>
-        <Image src={turing} className="h-2/3 w-auto mt-5" />
-      </div>
-    )
+    const personW =  person.split(" ").join("").toLowerCase()
+    const searchVal = personW.includes(input.toLowerCase())
+    console.log(input)
+    console.log(personW)
+    if(input===""){
+      return(
+        <div onClick={()=>{router.push(`/history/${person.split(" ").join("")}`)}} key={obj.name} className='mt-10 w-80 h-80 bg-white rounded-xl flex flex-col items-center transition transform duration-300 ease-in-out hover:scale-110 cursor-pointer'>
+          <p className='text-center text-lg mt-5'>{person}</p>
+          <img src={obj.src} className="rounded-xl mt-5 w-5/6 h-2/3" />
+          <p className='text-center'>---{obj.description}---</p>
+        </div>
+      )
+    } if(searchVal) {
+      return(
+        <div onClick={()=>{router.push(`/history/${person.split(" ").join("")}`)}} key={obj.name} className=' mt-10 w-80 h-80 bg-white rounded-xl flex flex-col items-center transition transform duration-300 ease-in-out hover:scale-110 cursor-pointer' >
+          <p className='text-center text-lg mt-5'>{person}</p>
+          <img src={obj.src} className="rounded-xl mt-5 w-5/6 h-2/3" />
+          <p className='text-center'>---{obj.description}---</p>
+        </div>
+      )
+    }
   }) 
   return (
-    <>
-      <input type='text' className='bg-white w-1/5 h-10 rounded-xl mx-auto mt-7 border-black'/>
+    <> 
+      <div className='mt-10 flex justify-center items-center'>
+        <span className='mr-3 text-lg font-semibold'>{"Find a hero =>"}</span>
+        <input type='text' onChange={handleChange} placeholder='Search' className='bg-white w-1/5 h-12 rounded-xl border-2 pl-2 text-lg font-bold'/>
+        <IoIosSearch className='ml-4 text-3xl'/>
+      </div>
       <div className=' mx-auto w-4/5 h-full grid grid-cols-3 gap-4'>
         {elements}
-        <div className='h-20 w-full bg-whitesmoke'>
+      <div className='h-20 w-full bg-whitesmoke'>
 
         </div>
       </div>
-    </>
+    </>  
   )
 }
 
